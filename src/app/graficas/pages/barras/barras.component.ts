2291ca0685d55bc0d08b16a23ce0d09cde93a55a
Fabio <fabioaoad@gmail.com>
2021-11-12 19:23:17 -0300

    Personalizando la gráfica  y añadiendo colores de adobe - Graficas - Seccion 22, apartado 9

--- src/app/graficas/pages/barras/barras.component.ts
+++ src/app/graficas/pages/barras/barras.component.ts
@@ -12,22 +12,17 @@ export class BarrasComponent implements OnInit {
 
   public barChartOptions: ChartOptions = {
     responsive: true,
-    // We use these empty structures as placeholders for dynamic theming.
-    scales: { xAxes: [{}], yAxes: [{}] },
-    plugins: {
-      datalabels: {
-        anchor: 'end',
-        align: 'end',
-      }
-    }
   };
-  public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  public barChartType: ChartType = 'bar';
-  public barChartLegend = true;
+  public barChartLabels: Label[] = ['2020', '2021', '2022', '2023', '2024', '2025', '2026'];
+  public barChartType: ChartType = 'bar'; // probar con horizontalBar
+  public barChartLegend = true;  // porbar con false
 
+
+  // Para crear colores: https://color.adobe.com/es/create/color-wheel
   public barChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor: '#0CE8AA', hoverBackgroundColor: 'red' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', backgroundColor: '#00F525', hoverBackgroundColor: 'red' },
+    { data: [8, 65, 32, 59, 11, 66, 100], label: 'Series C', backgroundColor: '#DDFA87', hoverBackgroundColor: 'red' },
   ];
 
   constructor() { }
@@ -36,17 +31,50 @@ export class BarrasComponent implements OnInit {
   }
 
 
-  public randomize(): void {
-    // Only Change 3 values
-    this.barChartData[0].data = [
-      Math.round(Math.random() * 100),
-      59,
-      80,
-      (Math.random() * 100),
-      56,
-      (Math.random() * 100),
-      40 ];
-  }
+  // public randomize(): void {
+  //   // Only Change 3 values
+  //   this.barChartData[0].data = [
+  //     Math.round(Math.random() * 100),
+  //     59,
+  //     80,
+  //     (Math.random() * 100),
+  //     56,
+  //     (Math.random() * 100),
+  //     40 ];
+  // }
 
+
+  public randomize(): void {
+    this.barChartData[0].data = [
+      Math.round( Math.random() * 100),
+      Math.round( Math.random() * 100),
+      Math.round( Math.random() * 100),
+      Math.round( Math.random() * 100),
+      Math.round( Math.random() * 100),
+      Math.round( Math.random() * 100),
+      Math.round( Math.random() * 100),
+    ];
+
+    this.barChartData[1].data = [
+      Math.round( Math.random() * 100),
+      Math.round( Math.random() * 100),
+      Math.round( Math.random() * 100),
+      Math.round( Math.random() * 100),
+      Math.round( Math.random() * 100),
+      Math.round( Math.random() * 100),
+      Math.round( Math.random() * 100),
+    ];
+
+    this.barChartData[2].data = [
+      Math.round( Math.random() * 100),
+      Math.round( Math.random() * 100),
+      Math.round( Math.random() * 100),
+      Math.round( Math.random() * 100),
+      Math.round( Math.random() * 100),
+      Math.round( Math.random() * 100),
+      Math.round( Math.random() * 100),
+    ];
+
+  }
 
 }
