781ab2817c222fa2c5c40530ec5f72bae6c15e9a
Fabio <fabioaoad@gmail.com>
2021-11-12 19:58:38 -0300

    Gráfica de Dona- Graficas - Seccion 22, apartado 12

--- src/app/graficas/pages/dona/dona.component.ts
+++ src/app/graficas/pages/dona/dona.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {Color, Label, MultiDataSet} from "ng2-charts";
+import {ChartType} from "chart.js";
 
 @Component({
   selector: 'app-dona',
@@ -6,11 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DonaComponent implements OnInit {
+export class DonaComponent{
 
-  constructor() { }
+  public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales', 'Otros'];
+  // public doughnutChartData: MultiDataSet = [
+  //   [350, 450, 100],
+  //   [50, 150, 120],
+  //   [250, 130, 70],
+  // ];
+  public doughnutChartData: MultiDataSet = [
+    [350, 450, 100, 180]
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
 
-  ngOnInit(): void {
-  }
+  public colors: Color[] = [
+    {
+      backgroundColor: [
+        '#1B68F5',
+        '#36D6D4',
+        '#3AEB6B',
+        '#7CD61A',
+      ]
+    }
+  ];
 
 }
