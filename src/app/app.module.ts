78772329ff7e0257fe02e837d6b4790e6306f4c9
Fabio <fabioaoad@gmail.com>
2021-11-12 20:08:00 -0300

    Gráfica de Dona mediante petición Http - Graficas - Seccion 22, apartado 13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './shared/menu/menu.component';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { MenuComponent } from './shared/menu/menu.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
