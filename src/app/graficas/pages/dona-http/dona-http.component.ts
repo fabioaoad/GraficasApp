5b0f79578d2fe7bc77fdeb7645ece4d3bc25935f
Fabio <fabioaoad@gmail.com>
2021-11-12 20:21:30 -0300

    Mostrar la información en la gráfica dona http - Graficas - Seccion 22, apartado 14

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {GraficaService} from "../../services/grafica.service";
+import {Color, Label, MultiDataSet} from "ng2-charts";
+import {ChartType} from "chart.js";
 
 @Component({
   selector: 'app-dona-http',
@@ -8,6 +10,25 @@ import {GraficaService} from "../../services/grafica.service";
   ]
 })
 export class DonaHttpComponent implements OnInit {
+  public doughnutChartLabels: Label[] = [
+
+  ];
+
+  public doughnutChartData: MultiDataSet = [
+
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
+
+  public colors: Color[] = [
+    {
+      backgroundColor: [
+        '#1B68F5',
+        '#36D6D4',
+        '#3AEB6B',
+        '#7CD61A',
+      ]
+    }
+  ];
 
   constructor( private graficaService: GraficaService ) { }
 
@@ -16,6 +37,12 @@ export class DonaHttpComponent implements OnInit {
     this.graficaService.getUsuariosRedesSociales()
       .subscribe( data => {
         console.log(data);
+        const labels = Object.keys( data );
+        const values = Object.values( data );
+        //console.log(labels);
+        //console.log(values);
+        this.doughnutChartLabels = labels;
+        this.doughnutChartData.push( values );
       });
   }
 
